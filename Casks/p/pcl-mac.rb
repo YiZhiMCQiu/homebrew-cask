a428c07a91e38ce54e320b9c96c4c3c5beb129c4
Minecraft-温迪 <124678228+YiZhiMCQiu@users.noreply.github.com>
2025-06-02 11:52:56 +0800

    add PCL-Mac

---
+++ Casks/p/pcl-mac.rb
@@ -0,0 +1,11 @@
+cask "pcl-mac" do
+  version "1.0.0"
+  sha256 "e96df1326b7bbbf740175e60c6ee9edba38c81e163644d3ab060f391e1af44ac"
+
+  url "https://gitee.com/yizhimcqiu/homebrew-pcl-mac/raw/master/PCL-Mac-1.0.0.zip"
+  name "PCL-Mac"
+  desc "一个使用 SwiftUI 框架重写的 PCL，专为 macOS 平台打造。"
+  homepage "https://github.com/PCL-Community/PCL-Mac"
+
+  app "PCL-Mac.app"
+end
